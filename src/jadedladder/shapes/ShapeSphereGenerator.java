7f93fd3379c59a9fc98f4da76951a4d71300f432
Josh Lloyd <j.nevercast@gmail.com>
2013-07-22 20:57:38 +1200

    Split sphere creation in to an Octant based helper This will allow us to make domes with ease in future

--- src/jadedladder/shapes/ShapeSphereGenerator.java
+++ src/jadedladder/shapes/ShapeSphereGenerator.java
@@ -3,59 +3,14 @@ package jadedladder.shapes;
 import jadedladder.JadedLadder;
 import jadedladder.common.IShapeGenerator;
 import jadedladder.common.IShapeable;
+import jadedladder.utils.GeometryUtils;
 import jadedladder.utils.MathUtils;
+import jadedladder.utils.GeometryUtils.Octant;
 
 public class ShapeSphereGenerator implements IShapeGenerator {
-
+	
 	@Override
-	public void generateShape(int radiusX, int radiusY, int radiusZ,
-			IShapeable shapeable) {
-
-		
-        final double invRadiusX = 1.0 / radiusX;
-        final double invRadiusY = 1.0 / radiusY;
-        final double invRadiusZ = 1.0 / radiusZ;
-       
-        double nextXn = 0;
-        forX: for (int x = 0; x <= radiusX; ++x) {
-            final double xn = nextXn;
-            nextXn = (x + 1) * invRadiusX;
-            double nextYn = 0;
-            forY: for (int y = 0; y <= radiusY; ++y) {
-                final double yn = nextYn;
-                nextYn = (y + 1) * invRadiusY;
-                double nextZn = 0;
-                forZ: for (int z = 0; z <= radiusZ; ++z) {
-                    final double zn = nextZn;
-                    nextZn = (z + 1) * invRadiusZ;
-
-                    double distanceSq = MathUtils.lengthSq(xn, yn, zn);
-                    if (distanceSq > 1) {
-                        if (z == 0) {
-                            if (y == 0) {
-                                break forX;
-                            }
-                            break forY;
-                        }
-                        break forZ;
-                    }
-
-                    if (MathUtils.lengthSq(nextXn, yn, zn) <= 1 && MathUtils.lengthSq(xn, nextYn, zn) <= 1 && MathUtils.lengthSq(xn, yn, nextZn) <= 1) {
-                        continue;
-                    }
-                    
-                    shapeable.setBlock(x, y, z);
-                    shapeable.setBlock(-x, y, z);
-                    shapeable.setBlock(x, -y, z);
-                    shapeable.setBlock(x, y, -z);
-                    shapeable.setBlock(-x, -y, z);
-                    shapeable.setBlock(x, -y, -z);
-                    shapeable.setBlock(-x, y, -z);
-                    shapeable.setBlock(-x, -y, -z);
-                }
-            }
-        }
-
+	public void generateShape(int radiusX, int radiusY, int radiusZ, IShapeable shapeable) {
+		GeometryUtils.makeSphere(radiusX, radiusY, radiusZ, shapeable, Octant.ALL);
 	}
-
 }
--- src/jadedladder/utils/GeometryUtils.java
+++ src/jadedladder/utils/GeometryUtils.java
@@ -1,45 +1,158 @@
 package jadedladder.utils;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 import jadedladder.common.IShapeable;
 import net.minecraftforge.common.ForgeDirection;
 
 public class GeometryUtils {
-	
+
+	public enum Octant {
+		TopSouthWest(-1, 1, 1, "Top South West"), 
+		TopNorthEast(1, 1, -1, "Top North East"), 
+		TopNorthWest(1, 1, 1, "Top North West"), 
+		TopSouthEast(-1, 1, -1, "Top South East"), 
+		BottomSouthWest(-1, -1, 1, "Bottom South West"), 
+		BottomNorthEast(1, -1, -1, "Bottom North East"), 
+		BottomNorthWest(1, -1, 1, "Bottom North West"), 
+		BottomSouthEast(-1, -1, -1, "Bottom South East");
+
+		public static final EnumSet ALL = EnumSet.allOf(Octant.class);
+		public static final EnumSet TOP = EnumSet.of(Octant.TopSouthEast, Octant.TopSouthWest, Octant.TopNorthEast, Octant.TopNorthWest);
+		public static final EnumSet BOTTOM = EnumSet.of(Octant.BottomSouthEast, Octant.BottomSouthWest, Octant.BottomNorthEast, Octant.BottomNorthWest);
+		
+		private final int x, y, z;
+		private final String name;
+
+		public int getXOffset() {
+			return x;
+		}
+
+		public int getYOffset() {
+			return y;
+		}
+
+		public int getZOffset() {
+			return z;
+		}
+
+		public String getFriendlyName() {
+			return name;
+		}
+
+		Octant(int x, int y, int z, String friendlyName) {
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.name = friendlyName;
+		}
+	}
+
 	/**
 	 * Makes a link of blocks in a shape
-	 * @param startX X position of origin
-	 * @param startY Y position of origin
-	 * @param startZ Z position of origin
-	 * @param direction Direction to apply the line
-	 * @param length Length of the line
-	 * @param shapeable Shapeable object to set the blocks
+	 * 
+	 * @param startX
+	 *            X position of origin
+	 * @param startY
+	 *            Y position of origin
+	 * @param startZ
+	 *            Z position of origin
+	 * @param direction
+	 *            Direction to apply the line
+	 * @param length
+	 *            Length of the line
+	 * @param shapeable
+	 *            Shapeable object to set the blocks
 	 */
-	public static void makeLine(int startX, int startY, int startZ, ForgeDirection direction, int length, IShapeable shapeable){
-		if(length == 0) return;
-		for(int offset = 0; offset <= length; offset++) /* Create a line in the direction of direction, length in size */
-			shapeable.setBlock(startX + (offset * direction.offsetX), startY + (offset * direction.offsetY), startZ + (offset * direction.offsetZ));
+	public static void makeLine(int startX, int startY, int startZ,
+			ForgeDirection direction, int length, IShapeable shapeable) {
+		if (length == 0)
+			return;
+		for (int offset = 0; offset <= length; offset++)
+			/* Create a line in the direction of direction, length in size */
+			shapeable.setBlock(startX + (offset * direction.offsetX), startY
+					+ (offset * direction.offsetY), startZ
+					+ (offset * direction.offsetZ));
 	}
-	
+
 	/**
 	 * Makes a flat plane along two directions
-	 * @param startX X position of origin
-	 * @param startY Y position of origin
-	 * @param startZ Z position of origin
-	 * @param width Width of the plane
-	 * @param height Height of the plane
-	 * @param right The right(width) direction of the plane
-	 * @param up The up(height) direction of the plane
-	 * @param shapeable The shape to apply the blocks to
+	 * 
+	 * @param startX
+	 *            X position of origin
+	 * @param startY
+	 *            Y position of origin
+	 * @param startZ
+	 *            Z position of origin
+	 * @param width
+	 *            Width of the plane
+	 * @param height
+	 *            Height of the plane
+	 * @param right
+	 *            The right(width) direction of the plane
+	 * @param up
+	 *            The up(height) direction of the plane
+	 * @param shapeable
+	 *            The shape to apply the blocks to
 	 */
-	public static void makePlane(int startX, int startY, int startZ, int width, int height, ForgeDirection right, ForgeDirection up, IShapeable shapeable){
-		if(width == 0 || height == 0) return;
-		int lineOffsetX, lineOffsetY, lineOffsetZ; 
+	public static void makePlane(int startX, int startY, int startZ, int width,
+			int height, ForgeDirection right, ForgeDirection up,
+			IShapeable shapeable) {
+		if (width == 0 || height == 0)
+			return;
+		int lineOffsetX, lineOffsetY, lineOffsetZ;
 		// We offset each line by up, and then apply it right
-		for(int h = 0; h <= height; h++){
+		for (int h = 0; h <= height; h++) {
 			lineOffsetX = startX + (h * up.offsetX);
 			lineOffsetY = startY + (h * up.offsetY);
 			lineOffsetZ = startZ + (h * up.offsetZ);
-			makeLine(lineOffsetX, lineOffsetY, lineOffsetZ, right, width, shapeable);
+			makeLine(lineOffsetX, lineOffsetY, lineOffsetZ, right, width,
+					shapeable);
 		}
+	}
+	
+	public static void makeSphere(int radiusX, int radiusY, int radiusZ, IShapeable shapeable, EnumSet octants) {
+
+        final double invRadiusX = 1.0 / radiusX;
+        final double invRadiusY = 1.0 / radiusY;
+        final double invRadiusZ = 1.0 / radiusZ;
+       
+        final Set<Octant> octantSet = octants;
+        
+        double nextXn = 0;
+        forX: for (int x = 0; x <= radiusX; ++x) {
+            final double xn = nextXn;
+            nextXn = (x + 1) * invRadiusX;
+            double nextYn = 0;
+            forY: for (int y = 0; y <= radiusY; ++y) {
+                final double yn = nextYn;
+                nextYn = (y + 1) * invRadiusY;
+                double nextZn = 0;
+                forZ: for (int z = 0; z <= radiusZ; ++z) {
+                    final double zn = nextZn;
+                    nextZn = (z + 1) * invRadiusZ;
+
+                    double distanceSq = MathUtils.lengthSq(xn, yn, zn);
+                    if (distanceSq > 1) {
+                        if (z == 0) {
+                            if (y == 0) {
+                                break forX;
+                            }
+                            break forY;
+                        }
+                        break forZ;
+                    }
+
+                    if (MathUtils.lengthSq(nextXn, yn, zn) <= 1 && MathUtils.lengthSq(xn, nextYn, zn) <= 1 && MathUtils.lengthSq(xn, yn, nextZn) <= 1) {
+                        continue;
+                    }
+                    
+                    for(Octant octant : octantSet){
+                    	shapeable.setBlock(x * octant.getXOffset(), y * octant.getYOffset(), z * octant.getZOffset());
+                    }
+                }
+            }
+        }
 	}
 }
