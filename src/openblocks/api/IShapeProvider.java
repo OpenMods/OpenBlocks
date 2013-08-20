292a848c51be7bee66d7169b7259892543304e3c
Mikee Franklin <mikeefranklin@gmail.com>
2013-08-20 10:06:53 +0100

    comments

--- src/openblocks/api/IShapeProvider.java
+++ src/openblocks/api/IShapeProvider.java
@@ -4,6 +4,11 @@ import net.minecraft.util.ChunkCoordinates;
 
 public interface IShapeProvider {
 	
+	/**
+	 * Gets a list of the coordinates for the current shape. All coordinates are
+	 * absolute coordinates.
+	 * @return An array of coordinates
+	 */
 	public ChunkCoordinates[] getShapeCoordinates();
 	
 }
