803d2d28a810f46f9e40c1ccbe90b6a01b2c4a37
Mikee Franklin <mikeefranklin@gmail.com>
2013-09-03 20:37:36 +0100

    Messing with cannon model

--- src/openblocks/client/model/ModelCannon.java
+++ src/openblocks/client/model/ModelCannon.java
@@ -7,74 +7,98 @@ import openblocks.common.tileentity.TileEntityCannon;
 
 public class ModelCannon extends ModelBase
 {
-	ModelRenderer main;
-	ModelRenderer gun4;
-	ModelRenderer gun1;
-	ModelRenderer gun2;
-	ModelRenderer gun3;
 	ModelRenderer shaft;
+	ModelRenderer shaft2;
+	ModelRenderer main;
+	ModelRenderer stand;
 	ModelRenderer base;
+	ModelRenderer pipe1;
+	ModelRenderer pipe2;
+	ModelRenderer maintop;
+	ModelRenderer rest;
+	ModelRenderer handle;
 
 	public ModelCannon()
 	{
 		textureWidth = 64;
 		textureHeight = 32;
 
-		main = new ModelRenderer(this, 0, 0);
-		main.addBox(-7F, -2F, -2F, 14, 5, 5);
-		main.setRotationPoint(0F, 6F, 0F);
-		main.setTextureSize(64, 32);
-		main.mirror = true;
-		setRotation(main, 0F, 0F, 0F);
-		gun4 = new ModelRenderer(this, 0, 10);
-		gun4.addBox(4.5F, -1.5F, -8F, 1, 1, 6);
-		gun4.setRotationPoint(0F, 6F, 0F);
-		gun4.setTextureSize(64, 32);
-		gun4.mirror = true;
-		setRotation(gun4, 0F, 0F, 0F);
-		gun1 = new ModelRenderer(this, 0, 10);
-		gun1.addBox(-5.5F, -1.5F, -8F, 1, 1, 6);
-		gun1.setRotationPoint(0F, 6F, 0F);
-		gun1.setTextureSize(64, 32);
-		gun1.mirror = true;
-		setRotation(gun1, 0F, 0F, 0F);
-		gun2 = new ModelRenderer(this, 0, 10);
-		gun2.addBox(-5.5F, 1.5F, -8F, 1, 1, 6);
-		gun2.setRotationPoint(0F, 6F, 0F);
-		gun2.setTextureSize(64, 32);
-		gun2.mirror = true;
-		setRotation(gun2, 0F, 0F, 0F);
-		gun3 = new ModelRenderer(this, 0, 10);
-		gun3.addBox(4.5F, 1.5F, -8F, 1, 1, 6);
-		gun3.setRotationPoint(0F, 6F, 0F);
-		gun3.setTextureSize(64, 32);
-		gun3.mirror = true;
-		setRotation(gun3, 0F, 0F, 0F);
-		shaft = new ModelRenderer(this, 0, 17);
-		shaft.addBox(-1F, 0F, -1F, 2, 8, 2);
-		shaft.setRotationPoint(0F, 7F, 0F);
+		shaft = new ModelRenderer(this, 12, 11);
+		shaft.addBox(-1F, 0.5F, 0F, 2, 1, 8);
+		shaft.setRotationPoint(0F, 5F, 0F);
 		shaft.setTextureSize(64, 32);
 		shaft.mirror = true;
 		setRotation(shaft, 0F, 0F, 0F);
-		base = new ModelRenderer(this, 8, 17);
-		base.addBox(-5F, 0F, -5F, 10, 1, 10);
-		base.setRotationPoint(0F, 15F, 0F);
+		shaft2 = new ModelRenderer(this, 0, 11);
+		shaft2.addBox(-1.5F, 0F, 5F, 3, 2, 1);
+		shaft2.setRotationPoint(0F, 5F, 0F);
+		shaft2.setTextureSize(64, 32);
+		shaft2.mirror = true;
+		setRotation(shaft2, 0F, 0F, 0F);
+		main = new ModelRenderer(this, 16, 20);
+		main.addBox(-1.5F, 0F, -5F, 3, 3, 6);
+		main.setRotationPoint(0F, 5F, 0F);
+		main.setTextureSize(64, 32);
+		main.mirror = true;
+		setRotation(main, 0F, 0F, 0F);
+		stand = new ModelRenderer(this, 0, 17);
+		stand.addBox(-0.5F, 1F, 0F, 1, 9, 1);
+		stand.setRotationPoint(0F, 5F, 0F);
+		stand.setTextureSize(64, 32);
+		stand.mirror = true;
+		setRotation(stand, -0.3490659F, 0F, 0F);
+		base = new ModelRenderer(this, 0, 0);
+		base.addBox(-5F, 9F, -5F, 10, 1, 10);
+		base.setRotationPoint(0F, 5F, 0F);
 		base.setTextureSize(64, 32);
 		base.mirror = true;
 		setRotation(base, 0F, 0F, 0F);
+		pipe1 = new ModelRenderer(this, 4, 17);
+		pipe1.addBox(0.5F, 1F, -6F, 2, 1, 1);
+		pipe1.setRotationPoint(0F, 5F, 0F);
+		pipe1.setTextureSize(64, 32);
+		pipe1.mirror = true;
+		setRotation(pipe1, 0F, 0F, 0F);
+		pipe2 = new ModelRenderer(this, 4, 19);
+		pipe2.addBox(1.5F, 1F, -5F, 1, 1, 1);
+		pipe2.setRotationPoint(0F, 5F, 0F);
+		pipe2.setTextureSize(64, 32);
+		pipe2.mirror = true;
+		setRotation(pipe2, 0F, 0F, 0F);
+		maintop = new ModelRenderer(this, 4, 21);
+		maintop.addBox(-1F, -0.5F, -4F, 2, 1, 4);
+		maintop.setRotationPoint(0F, 5F, 0F);
+		maintop.setTextureSize(64, 32);
+		maintop.mirror = true;
+		setRotation(maintop, 0F, 0F, 0F);
+		rest = new ModelRenderer(this, 0, 15);
+		rest.addBox(-2.5F, 7F, -0.2F, 5, 1, 1);
+		rest.setRotationPoint(0F, 5F, 0F);
+		rest.setTextureSize(64, 32);
+		rest.mirror = true;
+		setRotation(rest, -0.3490659F, 0F, 0F);
+		handle = new ModelRenderer(this, 0, 29);
+		handle.addBox(-5F, 1F, -2F, 10, 1, 1);
+		handle.setRotationPoint(0F, 5F, 0F);
+		handle.setTextureSize(64, 32);
+		handle.mirror = true;
+		setRotation(handle, 0F, 0F, 0F);
 	}
 
 	public void render(TileEntity te, float f) {
 
 		float f5 = 0.0625F;
 		setRotationAngles(te, f);
-		main.render(f5);
-		gun1.render(f5);
-		gun2.render(f5);
-		gun3.render(f5);
-		gun4.render(f5);
-		base.render(f5);
 		shaft.render(f5);
+		shaft2.render(f5);
+		main.render(f5);
+		stand.render(f5);
+		base.render(f5);
+		pipe1.render(f5);
+		pipe2.render(f5);
+		maintop.render(f5);
+		rest.render(f5);
+		handle.render(f5);
 	}
 
 	private void setRotation(ModelRenderer model, float x, float y, float z) {
@@ -85,13 +109,14 @@ public class ModelCannon extends ModelBase
 
 	public void setRotationAngles(TileEntity te, float f) {
 		TileEntityCannon cannon = (TileEntityCannon)te;
-		main.rotateAngleX = (float)Math.toRadians(cannon.pitch.getValue());
-		gun1.rotateAngleX = gun2.rotateAngleX = gun3.rotateAngleX = gun4.rotateAngleX = main.rotateAngleX;
+		main.rotateAngleX = (float)Math.toRadians(-cannon.pitch.getValue());
+		maintop.rotateAngleX = rest.rotateAngleX = stand.rotateAngleX = handle.rotateAngleX = shaft.rotateAngleX = shaft2.rotateAngleX = pipe1.rotateAngleX = pipe2.rotateAngleX = main.rotateAngleX;
 		main.rotateAngleZ = 0;
-		gun1.rotateAngleZ = gun2.rotateAngleZ = gun3.rotateAngleZ = gun4.rotateAngleZ = main.rotateAngleZ;
+		stand.rotateAngleZ = 0;
+		maintop.rotateAngleZ = rest.rotateAngleZ = handle.rotateAngleZ = shaft.rotateAngleZ = shaft2.rotateAngleZ = pipe1.rotateAngleZ = pipe2.rotateAngleZ = main.rotateAngleZ;
 
 		main.rotateAngleY = (float)Math.toRadians(cannon.yaw.getValue());
-		gun1.rotateAngleY = gun2.rotateAngleY = gun3.rotateAngleY = gun4.rotateAngleY = main.rotateAngleY;
+		maintop.rotateAngleY = rest.rotateAngleY = stand.rotateAngleY = handle.rotateAngleY = shaft.rotateAngleY = shaft2.rotateAngleY = pipe1.rotateAngleY = pipe2.rotateAngleY = main.rotateAngleY;
 	}
 
 }
