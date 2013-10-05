e85f919b59180fe912027088f37c2bc476015222
Mikee Franklin <mikeefranklin@gmail.com>
2013-10-05 16:14:07 +0100

    A bit more work on the tank level meter thing

--- src/openblocks/client/gui/component/GuiComponentTankLevel.java
+++ src/openblocks/client/gui/component/GuiComponentTankLevel.java
@@ -70,9 +70,18 @@ public class GuiComponentTankLevel extends Gui {
 		// bottom right corner
 		
 		if (stack != null && stack.getFluid() != null) {
-
 			Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.locationBlocksTexture);
-			this.drawTexturedModelRectFromIcon(x+3, y+3, stack.getFluid().getStillIcon(), width - 6, height - 6);
+			Tessellator tessellator = Tessellator.instance;
+	        tessellator.startDrawingQuads();
+	        Icon icon = stack.getFluid().getIcon();
+	        if (icon != null) {
+	        	double fluidHeight = (double)(height - 3) * percentFull;
+		        tessellator.addVertexWithUV((double)(x + 3), (double)(y + height - 3), (double)this.zLevel, (double)icon.getMinU(), (double)icon.getMaxV());
+		        tessellator.addVertexWithUV((double)(x + width - 3), (double)(y + height - 3), (double)this.zLevel, (double)icon.getMaxU(), (double)icon.getMaxV());
+		        tessellator.addVertexWithUV((double)(x + width - 3), (double)(y + (height - fluidHeight)), (double)this.zLevel, (double)icon.getMaxU(), (double)icon.getMinV());
+		        tessellator.addVertexWithUV((double)(x + 3), (double)(y  + (height - fluidHeight)), (double)this.zLevel, (double)icon.getMinU(), (double)icon.getMinV());
+		        tessellator.draw();
+	        }
 		}
 	}
 	
