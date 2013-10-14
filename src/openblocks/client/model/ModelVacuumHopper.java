effe79c60909da815f979f0cb72e7c77d72ee176
Mikee Franklin <mikeefranklin@gmail.com>
2013-10-14 20:08:05 +0100

    We're getting there! added colored bands

--- src/openblocks/client/model/ModelVacuumHopper.java
+++ src/openblocks/client/model/ModelVacuumHopper.java
@@ -9,8 +9,10 @@ import openblocks.sync.SyncableFlags;
 public class ModelVacuumHopper extends ModelBase {
 
 	ModelRenderer middle;
+	ModelRenderer outputItems;
+	ModelRenderer outputXP;
 	ModelRenderer output2;
-	ModelRenderer output;
+	ModelRenderer outputBoth;
 
 	public ModelVacuumHopper() {
 		textureWidth = 64;
@@ -22,63 +24,62 @@ public class ModelVacuumHopper extends ModelBase {
 		middle.setTextureSize(64, 32);
 		middle.mirror = true;
 		setRotation(middle, 0F, 0F, 0F);
+
 		output2 = new ModelRenderer(this, 0, 22);
 		output2.addBox(-2.5F, -5F, -2.5F, 5, 1, 5);
 		output2.setRotationPoint(0F, 8F, 0F);
 		output2.setTextureSize(64, 32);
 		output2.mirror = true;
-		setRotation(output2, 0F, 0F, 0F);
-		output = new ModelRenderer(this, 0, 16);
-		output.addBox(-1.5F, -8F, -1.5F, 3, 3, 3);
-		output.setRotationPoint(0F, 8F, 0F);
-		output.setTextureSize(64, 32);
-		output.mirror = true;
-		setRotation(output, 0F, 0F, 0F);
+		
+		outputBoth = new ModelRenderer(this, 0, 16);
+		outputBoth.addBox(-1.5F, -8F, -1.5F, 3, 3, 3);
+		outputBoth.setRotationPoint(0F, 8F, 0F);
+		outputBoth.setTextureSize(64, 32);
+		outputBoth.mirror = true;
+		setRotation(outputBoth, 0F, 0F, 0F);
+		
+		outputXP = new ModelRenderer(this, 12, 16);
+		outputXP.addBox(-1.5F, -8F, -1.5F, 3, 3, 3);
+		outputXP.setRotationPoint(0F, 8F, 0F);
+		outputXP.setTextureSize(64, 32);
+		outputXP.mirror = true;
+		setRotation(outputXP, 0F, 0F, 0F);
+		
+		outputItems = new ModelRenderer(this, 24, 16);
+		outputItems.addBox(-1.5F, -8F, -1.5F, 3, 3, 3);
+		outputItems.setRotationPoint(0F, 8F, 0F);
+		outputItems.setTextureSize(64, 32);
+		outputItems.mirror = true;
+		setRotation(outputItems, 0F, 0F, 0F);
 	}
 
+	private void renderValve(SyncableFlags itemOutputs, SyncableFlags xpOutputs, ForgeDirection direction, float rotX, float rotZ, float f5) {
+
+		boolean items = itemOutputs.get(direction);
+		boolean xp = xpOutputs.get(direction);
+		
+		if (items || xp) {
+			ModelRenderer valve = items && xp ? outputBoth : items ? outputItems : outputXP;
+			outputBoth.rotateAngleX = valve.rotateAngleX = rotX;
+			outputBoth.rotateAngleZ = valve.rotateAngleZ = rotZ;
+			output2.render(f5);
+			valve.render(f5);
+		}
+	}
+	
 	public void render(TileEntityVacuumHopper hopper, float f) {
 
 		float f5 = 0.0625F;
 		middle.render(f5);
 		SyncableFlags itemOutputs = hopper.getItemOutputs();
 		SyncableFlags xpOutputs = hopper.getXPOutputs();
-		if (itemOutputs.get(ForgeDirection.UP) || xpOutputs.get(ForgeDirection.UP)) {
-			output.rotateAngleX = output2.rotateAngleX = 0;
-			output.rotateAngleZ = output2.rotateAngleZ = 0;
-			output.render(f5);
-			output2.render(f5);
-		}
-		if (itemOutputs.get(ForgeDirection.DOWN) || xpOutputs.get(ForgeDirection.DOWN)) {
-			output.rotateAngleX = output2.rotateAngleX = (float)Math.toRadians(180);
-			output.rotateAngleZ = output2.rotateAngleZ = 0;
-			output.render(f5);
-			output2.render(f5);
-		}
-		if (itemOutputs.get(ForgeDirection.EAST) || xpOutputs.get(ForgeDirection.EAST)) {
-			output.rotateAngleX = output2.rotateAngleX = 0;
-			output.rotateAngleZ = output2.rotateAngleZ = (float)Math.toRadians(90);
-			output.render(f5);
-			output2.render(f5);
-		}
-		if (itemOutputs.get(ForgeDirection.WEST) || xpOutputs.get(ForgeDirection.WEST)) {
-			output.rotateAngleX = output2.rotateAngleX = 0;
-			output.rotateAngleZ = output2.rotateAngleZ = (float)Math.toRadians(-90);
-			output.render(f5);
-			output2.render(f5);
-		}
-		if (itemOutputs.get(ForgeDirection.NORTH) || xpOutputs.get(ForgeDirection.NORTH)) {
-			output.rotateAngleX = output2.rotateAngleX = (float)Math.toRadians(-90);
-			output.rotateAngleZ = output2.rotateAngleZ = 0;
-			output.render(f5);
-			output2.render(f5);
-		}
-		if (itemOutputs.get(ForgeDirection.SOUTH) || xpOutputs.get(ForgeDirection.SOUTH)) {
-			output.rotateAngleX = output2.rotateAngleX = (float)Math.toRadians(90);
-			output.rotateAngleZ = output2.rotateAngleZ = 0;
-			output.render(f5);
-			output2.render(f5);
-		}
-
+		renderValve(itemOutputs, xpOutputs, ForgeDirection.UP, 0, 0, f5);
+		renderValve(itemOutputs, xpOutputs, ForgeDirection.DOWN, (float)Math.toRadians(180), 0, f5);
+		renderValve(itemOutputs, xpOutputs, ForgeDirection.EAST, 0, (float)Math.toRadians(90), f5);
+		renderValve(itemOutputs, xpOutputs, ForgeDirection.WEST, 0, (float)Math.toRadians(-90), f5);
+		renderValve(itemOutputs, xpOutputs, ForgeDirection.NORTH, (float)Math.toRadians(-90), 0, f5);
+		renderValve(itemOutputs, xpOutputs, ForgeDirection.SOUTH, (float)Math.toRadians(90), 0, f5);
+		
 	}
 
 	private static void setRotation(ModelRenderer model, float x, float y, float z) {
