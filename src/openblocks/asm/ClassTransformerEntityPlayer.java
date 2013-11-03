2ce16b707762d5333cf5660e0d67121d72735ef0
Bartek Bok <bartek.bok@gmail.com>
2013-11-03 11:53:17 +0100

    Maybe?

--- src/openblocks/asm/ClassTransformerEntityPlayer.java
+++ src/openblocks/asm/ClassTransformerEntityPlayer.java
@@ -4,16 +4,22 @@ import net.minecraft.launchwrapper.IClassTransformer;
 
 import org.objectweb.asm.*;
 
+import cpw.mods.fml.common.asm.transformers.deobf.FMLDeobfuscatingRemapper;
+
 public class ClassTransformerEntityPlayer implements IClassTransformer {
 
+	private static final String isInBedDesc = "()Z";
 	private static final String isInBedNameMcp = "isInBed";
 	private static final String isInBedNameSrg = "func_71065_l";
-	private static final String bedHookCls = "openblocks/Hooks";
 
 	public static boolean IsInBedHookSuccess = false;
 
-	private static boolean shouldHook(String methodName) {
-		return methodName.equals(isInBedNameMcp) || methodName.equals(isInBedNameSrg);
+	private static boolean shouldHook(String clsName, String methodName, String methodDesc) {
+		if (!methodDesc.equals(isInBedDesc)) return false;
+		if (methodName.equals(isInBedNameMcp)) return true;
+
+		String mapped = FMLDeobfuscatingRemapper.INSTANCE.mapMethodName(clsName, methodName, methodDesc);
+		return mapped.equals(isInBedNameSrg);
 	}
 
 	private static class HookMethodVisitor extends MethodVisitor {
@@ -25,7 +31,7 @@ public class ClassTransformerEntityPlayer implements IClassTransformer {
 		public void visitCode() {
 			mv.visitCode();
 			mv.visitVarInsn(Opcodes.ALOAD, 0); // EntityPlayer this
-			mv.visitMethodInsn(Opcodes.INVOKESTATIC, bedHookCls, "isInBed", "(Lnet/minecraft/entity/player/EntityPlayer;)Z");
+			mv.visitMethodInsn(Opcodes.INVOKESTATIC, "openblocks/Hooks", "isInBed", "(Lnet/minecraft/entity/player/EntityPlayer;)Z");
 			Label skipReturn = new Label();
 			mv.visitJumpInsn(Opcodes.IFEQ, skipReturn);
 			mv.visitInsn(Opcodes.ICONST_1);
@@ -35,15 +41,18 @@ public class ClassTransformerEntityPlayer implements IClassTransformer {
 		}
 	}
 
-	public class HookClassVistor extends ClassVisitor {
-		public HookClassVistor(ClassVisitor cv) {
+	public class HookClassVisitor extends ClassVisitor {
+		private final String obfClassName;
+
+		public HookClassVisitor(String obfClassName, ClassVisitor cv) {
 			super(Opcodes.ASM4, cv);
+			this.obfClassName = obfClassName;
 		}
 
 		@Override
 		public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
 			MethodVisitor parent = super.visitMethod(access, name, desc, signature, exceptions);
-			return shouldHook(name)? new HookMethodVisitor(parent) : parent;
+			return shouldHook(obfClassName, name, desc)? new HookMethodVisitor(parent) : parent;
 		}
 	}
 
@@ -52,8 +61,8 @@ public class ClassTransformerEntityPlayer implements IClassTransformer {
 		if (!transformedName.equals("net.minecraft.entity.player.EntityPlayer")) { return bytes; }
 
 		ClassReader cr = new ClassReader(bytes);
-		ClassWriter cw = new ClassWriter(0);
-		HookClassVistor mod = new HookClassVistor(cw);
+		ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_FRAMES);
+		HookClassVisitor mod = new HookClassVisitor(name, cw);
 		cr.accept(mod, 0);
 		return cw.toByteArray();
 	}
