fb49960a08529ee499030b6440dc190c97d9c28d
Mikee Franklin <mikeefranklin@gmail.com>
2013-11-06 23:21:06 +0000

    dammmnnn

--- src/openblocks/common/DonationUrlManager.java
+++ src/openblocks/common/DonationUrlManager.java
@@ -2,6 +2,8 @@ package openblocks.common;
 
 import java.util.HashMap;
 
+import openblocks.Mods;
+
 public class DonationUrlManager {
 
 	protected static DonationUrlManager instance;
@@ -15,10 +17,37 @@ public class DonationUrlManager {
 	private HashMap<String, String> donationUrls = new HashMap<String, String>();
 
 	public DonationUrlManager() {
-		addUrl("OpenBlocks", "http://www.google.com");
-		addUrl("OpenPeripheral", "http://www.google.com");
-		addUrl("ComputerCraft", "http://www.computercraft.info/donate/");
-		addUrl("CCTurtle", "http://www.computercraft.info/donate/");
+		addUrl(Mods.OPENBLOCKS, "https://flattr.com/profile/openblocks");
+		addUrl(Mods.OPENPERIPHERAL, "https://flattr.com/profile/openblocks");
+		addUrl(Mods.COMPUTERCRAFT, "http://www.computercraft.info/donate/");
+		addUrl(Mods.COMPUTERCRAFT_TURTLE, "http://www.computercraft.info/donate/");
+		addUrl(Mods.TCONSTRUCT, "http://www.minecraftforum.net/topic/1659892-");
+		addUrl(Mods.ARSMAGICA2, "http://www.minecraftforum.net/topic/2028696-");
+		addUrl(Mods.APPLIED_ENERGISTICS, "http://ae-mod.info/");
+		addUrl(Mods.BIBLIOCRAFT, "http://www.bibliocraftmod.com/");
+		addUrl(Mods.BILLUND, "http://www.computercraft.info/donate/");
+		addUrl(Mods.EXTRABEES, "http://www.minecraftforum.net/topic/1324321-");
+		addUrl(Mods.EXTRATREES, "http://www.minecraftforum.net/topic/1324321-");
+		addUrl(Mods.BIOMESOPLENTY, "http://www.minecraftforge.net/forum/index.php/topic,13677.0.html");
+		addUrl(Mods.BUILDCRAFT_BUILDERS, "http://minecraft.curseforge.com/mc-mods/buildcraft/");
+		addUrl(Mods.BUILDCRAFT_CORE, "http://minecraft.curseforge.com/mc-mods/buildcraft/");
+		addUrl(Mods.BUILDCRAFT_ENERGY, "http://minecraft.curseforge.com/mc-mods/buildcraft/");
+		addUrl(Mods.BUILDCRAFT_FACTORY, "http://minecraft.curseforge.com/mc-mods/buildcraft/");
+		addUrl(Mods.BUILDCRAFT_SILICON, "http://minecraft.curseforge.com/mc-mods/buildcraft/");
+		addUrl(Mods.BUILDCRAFT_TRANSPORT, "http://minecraft.curseforge.com/mc-mods/buildcraft/");
+		addUrl(Mods.ENDERSTORAGE, "http://www.minecraftforum.net/topic/909223-");
+		addUrl(Mods.EXTRAUTILITIES, "http://www.minecraftforum.net/topic/1776056-");
+		addUrl(Mods.CHICKENCHUNKS, "http://www.minecraftforum.net/topic/909223-");
+		addUrl(Mods.GRAVITYGUN, "http://ichun.us/mods/");
+		addUrl(Mods.HATSTAND, "http://ichun.us/mods/");
+		addUrl(Mods.IC2, "http://wiki.industrial-craft.net/index.php?title=Main_Page");
+		addUrl(Mods.MAGICBEES, "https://flattr.com/profile/MysteriousAges");
+		addUrl(Mods.MPS, "http://machinemuse.net/");
+		addUrl(Mods.PORTALGUN, "http://ichun.us/mods/");
+		addUrl(Mods.RAILCRAFT, "http://railcraft.wikispaces.com/");
+		addUrl(Mods.STEVESCARTS, "http://stevescarts2.wikispaces.com/");
+		addUrl(Mods.TRANSLOCATOR, "http://www.minecraftforum.net/topic/909223-");
+		addUrl(Mods.WIRELESSREDSTONECBE, "http://www.minecraftforum.net/topic/909223-");
 	}
 
 	public void addUrl(String modId, String url) {
