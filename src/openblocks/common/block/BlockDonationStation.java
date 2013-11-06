920614c6f79f4a9c8a2f0e2a6f9a4b3cbb20bb53
Mikee Franklin <mikeefranklin@gmail.com>
2013-11-06 20:12:38 +0000

    pig

--- src/openblocks/common/block/BlockDonationStation.java
+++ src/openblocks/common/block/BlockDonationStation.java
@@ -3,6 +3,7 @@ package openblocks.common.block;
 import openblocks.Config;
 import openblocks.common.tileentity.TileEntityDonationStation;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 
 public class BlockDonationStation extends OpenBlock {
 
@@ -10,8 +11,14 @@ public class BlockDonationStation extends OpenBlock {
 		super(Config.blockDonationStationId, Material.ground);
 		setupBlock(this, "donationStation", TileEntityDonationStation.class);
 		setRotationMode(BlockRotationMode.FOUR_DIRECTIONS);
+		setBlockBounds(0.2f, 0.25f, 0.2f, 0.8f, 0.85f, 0.8f);
 	}
 
+	@Override
+	public void registerIcons(IconRegister registry) {
+		blockIcon = registry.registerIcon("openblocks:donationstation");
+	}
+	
 	@Override
 	public boolean isOpaqueCube() {
 		return false;
