789d2289afae7b091ec5c6f4db1c8ed6545f00a5
Josh Lloyd <j.nevercast@gmail.com>
2013-11-17 04:54:43 +1300

    Paint cans empty now

--- src/openblocks/common/tileentity/TileEntityPaintCan.java
+++ src/openblocks/common/tileentity/TileEntityPaintCan.java
@@ -3,6 +3,7 @@ package openblocks.common.tileentity;
 import java.util.Set;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.ForgeDirection;
@@ -12,6 +13,7 @@ import openblocks.common.api.IPlaceAwareTile;
 import openblocks.common.block.BlockPaintCan;
 import openblocks.sync.ISyncableObject;
 import openblocks.sync.SyncableInt;
+import openblocks.utils.BlockUtils;
 
 public class TileEntityPaintCan extends SyncedTileEntity implements IPlaceAwareTile, IActivateAwareTile {
 
@@ -35,7 +37,7 @@ public class TileEntityPaintCan extends SyncedTileEntity implements IPlaceAwareT
 
 	@Override
 	public boolean onBlockActivated(EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
-		if (!worldObj.isRemote) {
+		if (!worldObj.isRemote && amount.getValue() > 0) {
 			ItemStack heldStack = player.getHeldItem();
 			if (heldStack != null && heldStack.getItem().equals(OpenBlocks.Items.paintBrush)) {
 				NBTTagCompound tag = heldStack.getTagCompound();
@@ -45,14 +47,28 @@ public class TileEntityPaintCan extends SyncedTileEntity implements IPlaceAwareT
 				}
 				tag.setInteger("color", color.getValue());
 				heldStack.setItemDamage(0);
+				amount.setValue(amount.getValue() - 1);
 				worldObj.playSoundAtEntity(player, "liquid.swim", 0.1F, 1.2F);
 			}
+		}
+		if(amount.getValue() == 0 && !worldObj.isRemote) {
+			ItemStack item = new ItemStack(Item.bucketEmpty);
+			BlockUtils.dropItemStackInWorld(worldObj, xCoord, yCoord, zCoord, item);
+			worldObj.setBlock(xCoord, yCoord, zCoord, 0);
 		}
 		return false;
 	}
 
 	public int getColor() {
 		return color.getValue();
+	}
+	
+	public int getAmount() {
+		return amount.getValue();
+	}
+	
+	public void setAmount(int amt) {
+		amount.setValue(amt);
 	}
 
 }
