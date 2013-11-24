ab016882f7d7aca1a0c880202b9d95d1a4410760
Mikee Franklin <mikeefranklin@gmail.com>
2013-11-24 16:04:20 +0000

    mutant stuff

--- src/openblocks/common/entity/EntityMutant.java
+++ src/openblocks/common/entity/EntityMutant.java
@@ -3,20 +3,20 @@ package openblocks.common.entity;
 import java.util.HashMap;
 
 import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.*;
 import net.minecraft.entity.monster.EntityCreeper;
-import net.minecraft.entity.monster.EntityEnderman;
-import net.minecraft.entity.monster.EntitySpider;
-import net.minecraft.entity.monster.EntityZombie;
-import net.minecraft.entity.passive.EntityOcelot;
 import net.minecraft.entity.passive.EntityTameable;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 import openblocks.api.IMutant;
 import openblocks.api.IMutantDefinition;
 import openblocks.api.MutantRegistry;
 
+import com.google.common.base.Objects;
 import com.google.common.io.ByteArrayDataInput;
 import com.google.common.io.ByteArrayDataOutput;
 
@@ -24,21 +24,28 @@ import cpw.mods.fml.common.registry.IEntityAdditionalSpawnData;
 
 public class EntityMutant extends EntityTameable implements IEntityAdditionalSpawnData, IMutant {
 
+	private Class<? extends EntityLivingBase> head;
+	private Class<? extends EntityLivingBase> body;
+	private Class<? extends EntityLivingBase> arms;
+	private Class<? extends EntityLivingBase> wings;
+	private Class<? extends EntityLivingBase> legs;
+	private Class<? extends EntityLivingBase> tail;
+
 	public EntityMutant(World world) {
 		super(world);
 		setSize(0.6F, 1.8F);
 		getNavigator().setAvoidsWater(true);
 		this.tasks.addTask(0, new EntityAISwimming(this));
-        this.tasks.addTask(1, new EntityAIAttackOnCollide(this, 1.0D, true));
-        this.tasks.addTask(2, new EntityAIFollowOwner(this, 1.0D, 10.0F, 2.0F));
+		this.tasks.addTask(1, new EntityAIAttackOnCollide(this, 1.0D, true));
+		this.tasks.addTask(2, new EntityAIFollowOwner(this, 1.0D, 10.0F, 2.0F));
 		this.tasks.addTask(3, new EntityAIPanic(this, 1.25D));
 		this.tasks.addTask(4, new EntityAIWander(this, 1.0D));
 		this.tasks.addTask(5, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
 		this.tasks.addTask(6, new EntityAILookIdle(this));
-        this.targetTasks.addTask(0, new EntityAIOwnerHurtByTarget(this));
-        this.targetTasks.addTask(1, new EntityAIOwnerHurtTarget(this));
-        this.targetTasks.addTask(3, new EntityAIHurtByTarget(this, true));
-        setTamed(true);
+		this.targetTasks.addTask(0, new EntityAIOwnerHurtByTarget(this));
+		this.targetTasks.addTask(1, new EntityAIOwnerHurtTarget(this));
+		this.targetTasks.addTask(3, new EntityAIHurtByTarget(this, true));
+		setTamed(true);
 	}
 
 	@Override
@@ -60,42 +67,61 @@ public class EntityMutant extends EntityTameable implements IEntityAdditionalSpa
 
 	@Override
 	public void writeSpawnData(ByteArrayDataOutput data) {
-		data.writeInt(0);
+		data.writeUTF(getEntityIdForClass(head));
+		data.writeUTF(getEntityIdForClass(body));
+		data.writeUTF(getEntityIdForClass(arms));
+		data.writeUTF(getEntityIdForClass(wings));
+		data.writeUTF(getEntityIdForClass(legs));
+		data.writeUTF(getEntityIdForClass(tail));
 	}
 
 	@Override
 	public void readSpawnData(ByteArrayDataInput data) {
-		data.readInt();
+		head = getEntityClassForId(data.readUTF());
+		body = getEntityClassForId(data.readUTF());
+		arms = getEntityClassForId(data.readUTF());
+		wings = getEntityClassForId(data.readUTF());
+		legs = getEntityClassForId(data.readUTF());
+		tail = getEntityClassForId(data.readUTF());
+	}
+
+	private String getEntityIdForClass(Class<? extends EntityLivingBase> klazz) {
+		return Objects.firstNonNull((String)EntityList.classToStringMapping.get(klazz), "");
+	}
+
+	@SuppressWarnings("unchecked")
+	private Class<? extends EntityLivingBase> getEntityClassForId(String id) {
+		return (Class<? extends EntityLivingBase>)EntityList.stringToClassMapping.get(id);
 	}
 
 	@Override
 	public IMutantDefinition getBody() {
-		return MutantRegistry.getDefinition(EntityEnderman.class);
+		return MutantRegistry.getDefinition(body);
 	}
 
 	@Override
 	public IMutantDefinition getHead() {
-		return MutantRegistry.getDefinition(EntityCreeper.class);
+		return MutantRegistry.getDefinition(head);
 	}
 
 	@Override
 	public IMutantDefinition getArms() {
-		return MutantRegistry.getDefinition(EntityZombie.class);
+		return MutantRegistry.getDefinition(arms);
 	}
 
 	@Override
 	public IMutantDefinition getWings() {
-		return MutantRegistry.getDefinition(EntityOcelot.class);
+		return MutantRegistry.getDefinition(wings);
 	}
 
 	@Override
 	public IMutantDefinition getLegs() {
-		return MutantRegistry.getDefinition(EntitySpider.class);
+		return MutantRegistry.getDefinition(legs);
 	}
 
 	@Override
 	public IMutantDefinition getTail() {
-		return MutantRegistry.getDefinition(EntityOcelot.class);
+		return MutantRegistry.getDefinition(tail);
 	}
 
 	@Override
@@ -119,8 +145,33 @@ public class EntityMutant extends EntityTameable implements IEntityAdditionalSpa
 	}
 
 	public void setTraitsFromMap(HashMap<String, Integer> dnas) {
-		// TODO Auto-generated method stub
-
+		head = EntityCreeper.class;
+		body = EntityCreeper.class;
+		arms = EntityCreeper.class;
+		legs = EntityCreeper.class;
+		wings = EntityCreeper.class;
+		tail = EntityCreeper.class;
 	}
 
+	@Override
+	public void writeEntityToNBT(NBTTagCompound tag) {
+		super.writeEntityToNBT(tag);
+		tag.setString("head", getEntityIdForClass(head));
+		tag.setString("body", getEntityIdForClass(body));
+		tag.setString("arms", getEntityIdForClass(arms));
+		tag.setString("legs", getEntityIdForClass(legs));
+		tag.setString("wings", getEntityIdForClass(wings));
+		tag.setString("tail", getEntityIdForClass(tail));
+	}
+
+	@Override
+	public void readEntityFromNBT(NBTTagCompound tag) {
+		super.readEntityFromNBT(tag);
+		head = getEntityClassForId(tag.getString("head"));
+		body = getEntityClassForId(tag.getString("body"));
+		arms = getEntityClassForId(tag.getString("arms"));
+		legs = getEntityClassForId(tag.getString("legs"));
+		wings = getEntityClassForId(tag.getString("wings"));
+		tail = getEntityClassForId(tag.getString("tail"));
+	}
 }
