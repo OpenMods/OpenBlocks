17cf870e0a133d169e950b40b4c061af0ed1d36d
Bartek Bok <bartek.bok@gmail.com>
2014-01-04 23:30:17 +0100

    Small cursor cleanup, make it side-sensitive

--- src/openblocks/common/item/ItemCursor.java
+++ src/openblocks/common/item/ItemCursor.java
@@ -10,6 +10,7 @@ import net.minecraft.world.World;
 import openblocks.Config;
 import openblocks.OpenBlocks;
 import openmods.utils.EnchantmentUtils;
+import openmods.utils.ItemUtils;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
@@ -33,67 +34,53 @@ public class ItemCursor extends Item {
 	}
 
 	@Override
-	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world,
-			int x, int y, int z, int par7, float par8, float par9, float par10) {
-		NBTTagCompound tag = new NBTTagCompound();
+	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
+		NBTTagCompound tag = ItemUtils.getItemTag(stack);
 		tag.setInteger("dimension", world.provider.dimensionId);
 		tag.setInteger("x", x);
 		tag.setInteger("y", y);
 		tag.setInteger("z", z);
-		stack.setTagCompound(tag);
+		tag.setInteger("side", side);
 		return true;
 	}
 
 	@Override
-	public ItemStack onItemRightClick(ItemStack itemStack, World world,
-			EntityPlayer player) {
+	public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player) {
 		if (world.isRemote) return itemStack;
+
 		NBTTagCompound tag = itemStack.getTagCompound();
-		if (tag != null) {
-			if (tag.hasKey("x") && tag.hasKey("y") && tag.hasKey("z")
-					&& tag.hasKey("dimension")) {
-				int x = tag.getInteger("x");
-				int y = tag.getInteger("y");
-				int z = tag.getInteger("z");
-				int dimension = tag.getInteger("dimension");
-				if (world.provider.dimensionId == dimension
-						&& world.blockExists(x, y, z)) {
-					int distance = (int)getDistanceToLinkedBlock(world, player, itemStack);
-					distance = Math.max(0, distance - 10);
-					int playerExperience = EnchantmentUtils.getPlayerXP(player);
-					if (distance > playerExperience) { return itemStack; }
-					int blockId = world.getBlockId(x, y, z);
-					Block block = Block.blocksList[blockId];
-					if (block != null) {
-						block.onBlockActivated(world, x, y, z, player, 0, 0, 0, 0);
-						EnchantmentUtils.drainPlayerXP(player, distance);
-					}
-				}
-			}
+		if (tag != null & tag.hasKey("x") && tag.hasKey("y") && tag.hasKey("z") && tag.hasKey("dimension")) {
+			final int x = tag.getInteger("x");
+			final int y = tag.getInteger("y");
+			final int z = tag.getInteger("z");
+			final int dimension = tag.getInteger("dimension");
+
+			if (world.provider.dimensionId == dimension && world.blockExists(x, y, z)) clickBlock(world, player, x, y, z, tag.getInteger("side"));
 		}
 		return itemStack;
 	}
 
-	public static double getDistanceToLinkedBlock(World world, EntityPlayer player, ItemStack stack) {
-		NBTTagCompound tag = stack.getTagCompound();
-		if (tag != null) {
-			if (tag.hasKey("x") && tag.hasKey("y") && tag.hasKey("z")
-					&& tag.hasKey("dimension")) {
-				int x = tag.getInteger("x");
-				int y = tag.getInteger("y");
-				int z = tag.getInteger("z");
-				int dimension = tag.getInteger("dimension");
-				if (dimension == world.provider.dimensionId) {
-					double xd = player.posX - x;
-					double yd = player.posY - y;
-					double zd = player.posZ - z;
-					return Math.sqrt(xd * xd + yd * yd + zd * zd);
-				} else {
-					return 0;
+	private static void clickBlock(World world, EntityPlayer player, final int x, final int y, final int z, int side) {
+		int blockId = world.getBlockId(x, y, z);
+		Block block = Block.blocksList[blockId];
+		if (block != null) {
+			if (player.capabilities.isCreativeMode) block.onBlockActivated(world, x, y, z, player, side, 0, 0, 0);
+			else {
+				final int cost = (int)Math.max(0, getDistanceToLinkedBlock(player, x, y, z) - 10);
+				final int playerExperience = EnchantmentUtils.getPlayerXP(player);
+				if (cost <= playerExperience) {
+					block.onBlockActivated(world, x, y, z, player, side, 0, 0, 0);
+					EnchantmentUtils.drainPlayerXP(player, cost);
 				}
 			}
 		}
-		return 0;
+	}
+
+	private static double getDistanceToLinkedBlock(EntityPlayer player, double x, double y, double z) {
+		double xd = player.posX - x;
+		double yd = player.posY - y;
+		double zd = player.posZ - z;
+		return Math.sqrt(xd * xd + yd * yd + zd * zd);
 	}
 
 }
