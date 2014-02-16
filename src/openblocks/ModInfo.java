41724d31493d3b039a84784a783edf7efcab1899
Mikee Franklin <mikeefranklin@gmail.com>
2014-02-16 21:55:48 +0000

    hardcoded dependancy version temporarily

--- src/openblocks/ModInfo.java
+++ src/openblocks/ModInfo.java
@@ -6,5 +6,5 @@ public class ModInfo {
 	public static final String VERSION = "@VERSION@";
 	public static final String PROXY_SERVER = "openblocks.common.ServerProxy";
 	public static final String PROXY_CLIENT = "openblocks.client.ClientProxy";
-	public static final String DEPENDENCIES = "required-after:OpenMods;after:OpenPeripheral";
+	public static final String DEPENDENCIES = "required-after:OpenMods@[0.2,];after:OpenPeripheral";
 }
