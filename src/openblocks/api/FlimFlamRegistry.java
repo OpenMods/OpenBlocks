23d2bbb71e29c4d56104c8af43c36d736758e1db
Bartek Bok <bartek.bok@gmail.com>
2014-03-16 16:41:34 +0100

    Preventing future confusion

--- src/openblocks/api/FlimFlamRegistry.java
+++ src/openblocks/api/FlimFlamRegistry.java
@@ -22,6 +22,8 @@ public class FlimFlamRegistry {
 		private final String name;
 		private final int cost;
 		private final int weight;
+		private int lowerLuck;
+		private int upperLuck;
 		private final IFlimFlamAction effect;
 
 		public FlimFlamMeta(String name, int cost, int weight, IFlimFlamAction effect) {
@@ -29,6 +31,8 @@ public class FlimFlamRegistry {
 			this.cost = cost;
 			this.weight = weight;
 			this.effect = effect;
+			if (cost < 0) setRange(Integer.MIN_VALUE, cost);
+			else setRange(cost, Integer.MAX_VALUE);
 		}
 
 		public FlimFlamMeta markSafe() {
@@ -38,6 +42,17 @@ public class FlimFlamRegistry {
 
 		public FlimFlamMeta markSilent() {
 			isSilent = true;
+			return this;
+		}
+
+		public FlimFlamMeta setRange(int a, int b) {
+			if (a < b) {
+				lowerLuck = a;
+				upperLuck = b;
+			} else {
+				lowerLuck = b;
+				upperLuck = a;
+			}
 			return this;
 		}
 
@@ -69,6 +84,11 @@ public class FlimFlamRegistry {
 		@Override
 		public IFlimFlamAction action() {
 			return effect;
+		}
+
+		@Override
+		public boolean canApply(int luck) {
+			return lowerLuck <= luck && luck <= upperLuck;
 		}
 	}
 
