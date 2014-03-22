e524910a00146848f515217c1165d4caa33eabf6
Seraphin- <ts.seraphin.osu@gmail.com>
2014-03-21 17:11:34 -0700

    More effects for witch trophy
    Why not

--- src/openblocks/trophy/WitchBehavior.java
+++ src/openblocks/trophy/WitchBehavior.java
@@ -7,10 +7,27 @@ import net.minecraft.util.ChatMessageComponent;
 import openblocks.common.tileentity.TileEntityTrophy;
 
 public class WitchBehavior implements ITrophyBehavior {
+static Random r = new Random();
 
 	@Override
-	public int executeActivateBehavior(TileEntityTrophy tile, EntityPlayer player) {
-		player.addPotionEffect(new PotionEffect(Potion.blindness.id, 70, 1));
+	public int executeActivateBehavior(TileEntityTrophy tile, EntityPlayer player){
+		switch(rand.nextInt(5) + 1){
+			case 1:
+				player.addPotionEffect(new PotionEffect(Potion.blindness.id, 70, 1));
+				break;
+			case 2:
+				player.addPotionEffect(new PotionEffect(Potion.hunger.id, 70, 1));
+				break;
+			case 3:
+				player.addPotionEffect(new PotionEffect(Potion.weakness.id, 70, 1));
+				break;
+			case 4:
+				player.addPotionEffect(new PotionEffect(Potion.poison.id, 70, 1));
+				break;
+			case 4:
+				player.addPotionEffect(new PotionEffect(Potion.harm.id, 1, 1));
+				break;
+		}
 		player.sendChatToPlayer(ChatMessageComponent.createFromTranslationKey("openblocks.misc.get_witched"));
 		return 0;
 	}
