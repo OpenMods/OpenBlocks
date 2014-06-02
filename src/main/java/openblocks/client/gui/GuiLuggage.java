bf6212f27d262ab04a2ce5b8fda9a299959ec588
Bartek Bok <bartek.bok@gmail.com>
2014-06-02 22:28:03 +0200

    So obvious

--- src/main/java/openblocks/client/gui/GuiLuggage.java
+++ src/main/java/openblocks/client/gui/GuiLuggage.java
@@ -1,12 +1,23 @@
 package openblocks.client.gui;
 
 import openblocks.common.container.ContainerLuggage;
+import openblocks.common.entity.EntityLuggage;
 import openmods.gui.BaseGuiContainer;
+
+import com.google.common.base.Strings;
 
 public class GuiLuggage extends BaseGuiContainer<ContainerLuggage> {
 
-	public GuiLuggage(ContainerLuggage container) {
-		super(container, 176, container.getOwner().isSpecial()? 221 : 167, "openblocks.gui.luggage");
+	private static String getInventoryName(EntityLuggage owner) {
+		String customName = owner.getCustomNameTag();
+		return Strings.isNullOrEmpty(customName)? "openblocks.gui.luggage" : customName;
 	}
 
+	private static int calculateHeight(EntityLuggage owner) {
+		return owner.isSpecial()? 221 : 167;
+	}
+
+	public GuiLuggage(ContainerLuggage container) {
+		super(container, 176, calculateHeight(container.getOwner()), getInventoryName(container.getOwner()));
+	}
 }
