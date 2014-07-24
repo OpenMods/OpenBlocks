b87108124988b653560247813af507f6c7c9dc23
Bartek Bok <bartek.bok@gmail.com>
2014-07-24 23:19:44 +0200

    Update build script

--- src/main/java/openblocks/OpenBlocks.java
+++ src/main/java/openblocks/OpenBlocks.java
@@ -70,7 +70,7 @@ public class OpenBlocks {
 	public static final String VERSION = "@VERSION@";
 	public static final String PROXY_SERVER = "openblocks.common.ServerProxy";
 	public static final String PROXY_CLIENT = "openblocks.client.ClientProxy";
-	public static final String DEPENDENCIES = "required-after:OpenMods@[0.5.1,];after:OpenPeripheral;after:NotEnoughCodecs";
+	public static final String DEPENDENCIES = "required-after:OpenMods@[0.6,];after:OpenPeripheral;after:NotEnoughCodecs";
 
 	private static final int ENTITY_HANGGLIDER_ID = 701;
 	private static final int ENTITY_LUGGAGE_ID = 702;
