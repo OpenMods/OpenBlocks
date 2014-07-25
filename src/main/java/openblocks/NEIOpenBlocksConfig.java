a03857c3226a8bdc7b53b498d731885bf571e5fb
Bartek Bok <bartek.bok@gmail.com>
2014-07-26 00:41:15 +0200

    Trying to reduce map spam, but NEI is too derpy

--- src/main/java/openblocks/NEIOpenBlocksConfig.java
+++ src/main/java/openblocks/NEIOpenBlocksConfig.java
@@ -1,7 +1,38 @@
 package openblocks;
 
-public class NEIOpenBlocksConfig /* implements IConfigureNEI */{
+import java.lang.reflect.Method;
 
-	// TODO
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
+import openblocks.OpenBlocks.Items;
+import codechicken.nei.api.IConfigureNEI;
+
+import com.google.common.base.Throwables;
+
+public class NEIOpenBlocksConfig implements IConfigureNEI {
+
+	@Override
+	public void loadConfig() {
+		try {
+			// I have no idea how to link with NEI API
+			Class<?> cls = Class.forName("codechicken.nei.api.API");
+			Method hide = cls.getMethod("hideItem", ItemStack.class);
+
+			if (Items.heightMap != null) hide.invoke(null, new ItemStack(Items.heightMap, 1, OreDictionary.WILDCARD_VALUE));
+			if (Items.tunedCrystal != null) hide.invoke(null, new ItemStack(Items.tunedCrystal, 1, OreDictionary.WILDCARD_VALUE));
+		} catch (Throwable t) {
+			Throwables.propagate(t);
+		}
+	}
+
+	@Override
+	public String getName() {
+		return "OpenBlocks-ItemHider";
+	}
+
+	@Override
+	public String getVersion() {
+		return "0.0";
+	}
 
 }
