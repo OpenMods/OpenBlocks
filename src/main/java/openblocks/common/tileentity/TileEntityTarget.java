a33feba5d1cda58cba7782b93c24734545a5e85f
Bartek Bok <bartek.bok@gmail.com>
2014-10-25 21:38:14 +0200

    Fix #520

--- src/main/java/openblocks/common/tileentity/TileEntityTarget.java
+++ src/main/java/openblocks/common/tileentity/TileEntityTarget.java
@@ -6,15 +6,21 @@ import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.init.Items;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.world.WorldServer;
 import net.minecraftforge.common.util.ForgeDirection;
 import openblocks.OpenBlocks;
 import openblocks.OpenBlocks.Blocks;
 import openblocks.OpenBlocks.ClassReferences;
+import openmods.Log;
 import openmods.api.INeighbourAwareTile;
 import openmods.api.ISurfaceAttachment;
+import openmods.fakeplayer.FakePlayerPool;
+import openmods.fakeplayer.FakePlayerPool.PlayerUserReturning;
+import openmods.fakeplayer.OpenModsFakePlayer;
 import openmods.sync.SyncableBoolean;
 import openmods.tileentity.SyncedTileEntity;
 import openmods.utils.BlockUtils;
@@ -95,35 +101,6 @@ public class TileEntityTarget extends SyncedTileEntity implements ISurfaceAttach
 		worldObj.notifyBlocksOfNeighborChange(xCoord, yCoord, zCoord, OpenBlocks.Blocks.target);
 	}
 
-	private void onRedstoneChanged() {
-		boolean isPowered = worldObj.isBlockIndirectlyGettingPowered(xCoord, yCoord, zCoord);
-
-		if (isPowered == isEnabled()) return;
-
-		if (!isPowered) {
-			final AxisAlignedBB aabb = AxisAlignedBB.getBoundingBox(xCoord - 0.1, yCoord - 0.1, zCoord - 0.1, xCoord + 1.1, yCoord + 1.1, zCoord + 1.1);
-
-			@SuppressWarnings("unchecked")
-			List<EntityArrow> arrows = worldObj.getEntitiesWithinAABB(EntityArrow.class, aabb);
-
-			int pickableCount = 0;
-
-			for (EntityArrow arrow : arrows) {
-				if (arrow.canBePickedUp == 1) pickableCount++;
-				arrow.setDead();
-			}
-
-			if (pickableCount > 0) {
-				ItemStack newStack = new ItemStack(Items.arrow, pickableCount, 0);
-				BlockUtils.dropItemStackInWorld(worldObj, xCoord, yCoord, zCoord, newStack);
-			}
-		}
-		worldObj.playSoundEffect(xCoord + 0.5, yCoord + 0.5, zCoord + 0.5, isPowered? "openblocks:target.open" : "openblocks:target.close", 0.5f, 1.0f);
-
-		setEnabled(isPowered);
-		sync();
-	}
-
 	@Override
 	public ForgeDirection getSurfaceDirection() {
 		return ForgeDirection.DOWN;
@@ -138,6 +115,61 @@ public class TileEntityTarget extends SyncedTileEntity implements ISurfaceAttach
 
 	@Override
 	public void onNeighbourChanged(Block block) {
-		onRedstoneChanged();
+		if (!(worldObj instanceof WorldServer)) return;
+		WorldServer world = (WorldServer)worldObj;
+
+		boolean isPowered = worldObj.isBlockIndirectlyGettingPowered(xCoord, yCoord, zCoord);
+
+		if (isPowered == isEnabled()) return;
+
+		dropArrowsAsItems(world);
+		worldObj.playSoundEffect(xCoord + 0.5, yCoord + 0.5, zCoord + 0.5, isPowered? "openblocks:target.open" : "openblocks:target.close", 0.5f, 1.0f);
+
+		setEnabled(isPowered);
+
+		sync();
+	}
+
+	private void dropArrowsAsItems(WorldServer world) {
+		final AxisAlignedBB aabb = AxisAlignedBB.getBoundingBox(xCoord - 0.1, yCoord - 0.1, zCoord - 0.1, xCoord + 1.1, yCoord + 1.1, zCoord + 1.1);
+
+		@SuppressWarnings("unchecked")
+		final List<EntityArrow> arrows = worldObj.getEntitiesWithinAABB(EntityArrow.class, aabb);
+
+		final List<ItemStack> drops = Lists.newArrayList();
+
+		int failed = FakePlayerPool.instance.executeOnPlayer(world, new PlayerUserReturning<Integer>() {
+
+			@Override
+			public Integer usePlayer(OpenModsFakePlayer fakePlayer) {
+				int failed = 0;
+
+				for (EntityArrow arrow : arrows) {
+					try {
+						arrow.onCollideWithPlayer(fakePlayer);
+					} catch (Throwable t) {
+						Log.warn(t, "Failed to collide arrow %s with fake player, returing vanilla one", arrow);
+						failed++;
+					}
+				}
+
+				IInventory inventory = fakePlayer.inventory;
+				for (int i = 0; i < inventory.getSizeInventory(); i++) {
+					ItemStack stack = inventory.getStackInSlot(i);
+					if (stack != null) {
+						drops.add(stack);
+						inventory.setInventorySlotContents(i, null);
+					}
+				}
+
+				return failed;
+
+			}
+		});
+
+		for (ItemStack drop : drops)
+			BlockUtils.dropItemStackInWorld(worldObj, xCoord, yCoord, zCoord, drop);
+
+		if (failed > 0) BlockUtils.dropItemStackInWorld(worldObj, xCoord, yCoord, zCoord, new ItemStack(Items.arrow, failed));
 	}
 }
