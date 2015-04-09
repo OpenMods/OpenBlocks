db2f5cc8eac70bb5d1a0c9094b9681b19e56ddc6
Bartek Bok <bartek.bok@gmail.com>
2015-04-09 21:37:15 +0200

    Allow minor versions of OpenModsLib

--- src/main/java/openblocks/OpenBlocks.java
+++ src/main/java/openblocks/OpenBlocks.java
@@ -63,7 +63,7 @@ public class OpenBlocks {
 	public static final String VERSION = "$VERSION$";
 	public static final String PROXY_SERVER = "openblocks.common.ServerProxy";
 	public static final String PROXY_CLIENT = "openblocks.client.ClientProxy";
-	public static final String DEPENDENCIES = "required-after:OpenMods@[$LIB-VERSION$]";
+	public static final String DEPENDENCIES = "required-after:OpenMods@[$LIB-VERSION$,$NEXT-LIB-VERSION$)";
 
 	private static final int ENTITY_HANGGLIDER_ID = 701;
 	private static final int ENTITY_LUGGAGE_ID = 702;
