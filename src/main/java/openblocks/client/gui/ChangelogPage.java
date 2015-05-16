36efc7a0df41805489335b84535638958dd45fe8
oldjunyi <archon.jk@hotmail.com>
2015-05-16 15:12:08 +0800

    update zh_CN.lang, make font scaling in book pages configurable

--- src/main/java/openblocks/client/gui/ChangelogPage.java
+++ src/main/java/openblocks/client/gui/ChangelogPage.java
@@ -29,21 +29,26 @@ public class ChangelogPage extends PageBase {
 	}
 
 	public ChangelogPage(String currentVersion, String section, List<String> lines) {
+		float scaleTitle = Float.parseFloat(StatCollector.translateToLocal("openmodslib.locale.scale.title"));
+		float scaleContent = Float.parseFloat(StatCollector.translateToLocal("openmodslib.locale.scale.content"));
+		int lineSpace = Integer.parseInt(StatCollector.translateToLocal("openmodslib.locale.lineSpace.titledPage"));
+		
 		section = StatCollector.translateToLocal(section);
 
 		FontRenderer renderer = Minecraft.getMinecraft().fontRenderer;
-		addComponent(new GuiComponentLabel(center(renderer, section), 24, section));
+		addComponent(new GuiComponentLabel(center(renderer, section), 24, section).setScale(scaleTitle));
 
 		int titleMiddle = center(renderer, currentVersion);
 
 		centerTitle = new GuiComponentLabel(titleMiddle, 12, currentVersion);
+		centerTitle.setScale(scaleTitle);
 		addComponent(centerTitle);
 
 		String contents = Joiner.on('\n').join(Iterables.transform(lines, BULLET_TRANSFORMER));
 
 		final GuiComponentLabel lblContent = new GuiComponentLabel(15, 40, getWidth() - 10, getHeight(), contents);
-		lblContent.setScale(0.5f);
-		lblContent.setAdditionalLineHeight(3);
+		lblContent.setScale(scaleContent);
+		lblContent.setAdditionalLineHeight(lineSpace);
 		addComponent(lblContent);
 	}
 
@@ -51,15 +56,17 @@ public class ChangelogPage extends PageBase {
 		FontRenderer renderer = Minecraft.getMinecraft().fontRenderer;
 		int leftSize = renderer.getStringWidth(name) / 2;
 		GuiComponentLabel bookmark = new GuiComponentLabel(centerTitle.getX() - 7 - leftSize, 14, "\u00a77" + name);
+		float scaleContent = Float.parseFloat(StatCollector.translateToLocal("openmodslib.locale.scale.content"));
 		bookmark.setListener(book.createBookmarkListener(page));
-		bookmark.setScale(0.5f);
+		bookmark.setScale(scaleContent);
 		addComponent(bookmark);
 	}
 
 	void addNextVersionBookmark(final GuiComponentBook book, String name, int page) {
 		GuiComponentLabel bookmark = new GuiComponentLabel(centerTitle.getX() + centerTitle.getWidth() + 7, 14, "\u00a77" + name);
+		float scaleContent = Float.parseFloat(StatCollector.translateToLocal("openmodslib.locale.scale.content"));
 		bookmark.setListener(book.createBookmarkListener(page));
-		bookmark.setScale(0.5f);
+		bookmark.setScale(scaleContent);
 		addComponent(bookmark);
 	}
 }
