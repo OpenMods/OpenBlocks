c856834735c1bee42b143a4f01376e7d98c7ab67
Bartek Bok <bartek.bok@gmail.com>
2016-01-31 17:17:52 +0100

    Allow API annotation to be placed on methods, expand docs

--- src/main/java/openblocks/api/ApiHolder.java
+++ src/main/java/openblocks/api/ApiHolder.java
@@ -6,12 +6,15 @@ import java.lang.annotation.*;
  * Static variables marked with this annotation will be filled with instance
  * of requested API (defined by type of variable).
  *
- * All variables must have type that implements {@link IApiInterface}.
+ * Static methods marked with this annotation will be called with instance of requested API.
+ * Methods must have single argument, which will be used to select API.
+ *
+ * All used types must implements {@link IApiInterface}.
  * If requested type is not provided by OpenPeripheralAddons, variable will not be set.
  *
  * All variables should be filled in 'init'. Value in 'preInit' is undefined.
  *
  */
-@Target(ElementType.FIELD)
+@Target({ ElementType.FIELD, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ApiHolder {}
