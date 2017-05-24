0d3878ffde4416d4682c8ff458e6d6c453cc1638
Bartek Bok <bartek.bok@gmail.com>
2017-05-24 20:37:46 +0200

    Fix lightmap on tank contents

--- src/main/java/openblocks/client/renderer/tileentity/TileEntityTankRenderer.java
+++ src/main/java/openblocks/client/renderer/tileentity/TileEntityTankRenderer.java
@@ -4,6 +4,8 @@ import net.minecraft.client.renderer.VertexBuffer;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
+import openblocks.Config;
 import openblocks.client.renderer.tileentity.tank.ITankRenderFluidData;
 import openblocks.common.tileentity.TileEntityTank;
 import openmods.utils.Diagonal;
@@ -19,17 +21,23 @@ public class TileEntityTankRenderer extends TileEntitySpecialRenderer<TileEntity
 
 		if (data != null && data.hasFluid()) {
 			buffer.setTranslation(x, y, z);
-			final float time = tankTile.getWorld().getTotalWorldTime() + partialTicks;
-			renderFluid(buffer, data, time);
+			final World world = tankTile.getWorld();
+			final float time = world.getTotalWorldTime() + partialTicks;
+			final int selfLightLevel = Config.tanksEmitLight? tankTile.getFluidLightLevel() : 0;
+			final int combinedLights = world.getCombinedLight(tankTile.getPos(), selfLightLevel);
+			renderFluid(buffer, data, time, combinedLights);
 			buffer.setTranslation(0, 0, 0);
 		}
 	}
 
-	private static void addVertexWithUV(VertexBuffer wr, double x, double y, double z, double u, double v) {
-		wr.pos(x, y, z).color(0xFF, 0xFF, 0xFF, 0xFF).tex(u, v).lightmap(240, 240).endVertex();
+	private static void addVertexWithUV(VertexBuffer wr, double x, double y, double z, double u, double v, int skyLight, int blockLight) {
+		wr.pos(x, y, z).color(0xFF, 0xFF, 0xFF, 0xFF).tex(u, v).lightmap(skyLight, blockLight).endVertex();
 	}
 
-	private static void renderFluid(final VertexBuffer wr, final ITankRenderFluidData data, float time) {
+	private static void renderFluid(final VertexBuffer wr, final ITankRenderFluidData data, float time, int combinedLights) {
+		final int skyLight = (combinedLights >> 16) & 0xFFFF;
+		final int blockLight = (combinedLights >> 0) & 0xFFFF;
+
 		final double se = data.getCornerFluidLevel(Diagonal.SE, time);
 		final double ne = data.getCornerFluidLevel(Diagonal.NE, time);
 		final double sw = data.getCornerFluidLevel(Diagonal.SW, time);
@@ -46,54 +54,54 @@ public class TileEntityTankRenderer extends TileEntitySpecialRenderer<TileEntity
 		final double vHeight = vMax - vMin;
 
 		if (data.shouldRenderFluidWall(EnumFacing.NORTH) && (nw > 0 || ne > 0)) {
-			addVertexWithUV(wr, 1, 0, 0, uMax, vMin);
-			addVertexWithUV(wr, 1, ne, 0, uMax, vMin + (vHeight * ne));
-			addVertexWithUV(wr, 0, nw, 0, uMin, vMin + (vHeight * nw));
-			addVertexWithUV(wr, 0, 0, 0, uMin, vMin);
+			addVertexWithUV(wr, 1, 0, 0, uMax, vMin, skyLight, blockLight);
+			addVertexWithUV(wr, 1, ne, 0, uMax, vMin + (vHeight * ne), skyLight, blockLight);
+			addVertexWithUV(wr, 0, nw, 0, uMin, vMin + (vHeight * nw), skyLight, blockLight);
+			addVertexWithUV(wr, 0, 0, 0, uMin, vMin, skyLight, blockLight);
 		}
 
 		if (data.shouldRenderFluidWall(EnumFacing.SOUTH) && (se > 0 || sw > 0)) {
-			addVertexWithUV(wr, 1, 0, 1, uMin, vMin);
-			addVertexWithUV(wr, 1, se, 1, uMin, vMin + (vHeight * se));
-			addVertexWithUV(wr, 0, sw, 1, uMax, vMin + (vHeight * sw));
-			addVertexWithUV(wr, 0, 0, 1, uMax, vMin);
+			addVertexWithUV(wr, 1, 0, 1, uMin, vMin, skyLight, blockLight);
+			addVertexWithUV(wr, 1, se, 1, uMin, vMin + (vHeight * se), skyLight, blockLight);
+			addVertexWithUV(wr, 0, sw, 1, uMax, vMin + (vHeight * sw), skyLight, blockLight);
+			addVertexWithUV(wr, 0, 0, 1, uMax, vMin, skyLight, blockLight);
 		}
 
 		if (data.shouldRenderFluidWall(EnumFacing.EAST) && (ne > 0 || se > 0)) {
-			addVertexWithUV(wr, 1, 0, 0, uMin, vMin);
-			addVertexWithUV(wr, 1, ne, 0, uMin, vMin + (vHeight * ne));
-			addVertexWithUV(wr, 1, se, 1, uMax, vMin + (vHeight * se));
-			addVertexWithUV(wr, 1, 0, 1, uMax, vMin);
+			addVertexWithUV(wr, 1, 0, 0, uMin, vMin, skyLight, blockLight);
+			addVertexWithUV(wr, 1, ne, 0, uMin, vMin + (vHeight * ne), skyLight, blockLight);
+			addVertexWithUV(wr, 1, se, 1, uMax, vMin + (vHeight * se), skyLight, blockLight);
+			addVertexWithUV(wr, 1, 0, 1, uMax, vMin, skyLight, blockLight);
 		}
 
 		if (data.shouldRenderFluidWall(EnumFacing.WEST) && (sw > 0 || nw > 0)) {
-			addVertexWithUV(wr, 0, 0, 1, uMin, vMin);
-			addVertexWithUV(wr, 0, sw, 1, uMin, vMin + (vHeight * sw));
-			addVertexWithUV(wr, 0, nw, 0, uMax, vMin + (vHeight * nw));
-			addVertexWithUV(wr, 0, 0, 0, uMax, vMin);
+			addVertexWithUV(wr, 0, 0, 1, uMin, vMin, skyLight, blockLight);
+			addVertexWithUV(wr, 0, sw, 1, uMin, vMin + (vHeight * sw), skyLight, blockLight);
+			addVertexWithUV(wr, 0, nw, 0, uMax, vMin + (vHeight * nw), skyLight, blockLight);
+			addVertexWithUV(wr, 0, 0, 0, uMax, vMin, skyLight, blockLight);
 		}
 
 		if (data.shouldRenderFluidWall(EnumFacing.UP)) {
 			final double uMid = (uMax + uMin) / 2;
 			final double vMid = (vMax + vMin) / 2;
 
-			addVertexWithUV(wr, 0.5, center, 0.5, uMid, vMid);
-			addVertexWithUV(wr, 1, se, 1, uMax, vMin);
-			addVertexWithUV(wr, 1, ne, 0, uMin, vMin);
-			addVertexWithUV(wr, 0, nw, 0, uMin, vMax);
+			addVertexWithUV(wr, 0.5, center, 0.5, uMid, vMid, skyLight, blockLight);
+			addVertexWithUV(wr, 1, se, 1, uMax, vMin, skyLight, blockLight);
+			addVertexWithUV(wr, 1, ne, 0, uMin, vMin, skyLight, blockLight);
+			addVertexWithUV(wr, 0, nw, 0, uMin, vMax, skyLight, blockLight);
 
-			addVertexWithUV(wr, 0, sw, 1, uMax, vMax);
-			addVertexWithUV(wr, 1, se, 1, uMax, vMin);
-			addVertexWithUV(wr, 0.5, center, 0.5, uMid, vMid);
-			addVertexWithUV(wr, 0, nw, 0, uMin, vMax);
+			addVertexWithUV(wr, 0, sw, 1, uMax, vMax, skyLight, blockLight);
+			addVertexWithUV(wr, 1, se, 1, uMax, vMin, skyLight, blockLight);
+			addVertexWithUV(wr, 0.5, center, 0.5, uMid, vMid, skyLight, blockLight);
+			addVertexWithUV(wr, 0, nw, 0, uMin, vMax, skyLight, blockLight);
 
 		}
 
 		if (data.shouldRenderFluidWall(EnumFacing.DOWN)) {
-			addVertexWithUV(wr, 1, 0, 0, uMax, vMin);
-			addVertexWithUV(wr, 1, 0, 1, uMin, vMin);
-			addVertexWithUV(wr, 0, 0, 1, uMin, vMax);
-			addVertexWithUV(wr, 0, 0, 0, uMax, vMax);
+			addVertexWithUV(wr, 1, 0, 0, uMax, vMin, skyLight, blockLight);
+			addVertexWithUV(wr, 1, 0, 1, uMin, vMin, skyLight, blockLight);
+			addVertexWithUV(wr, 0, 0, 1, uMin, vMax, skyLight, blockLight);
+			addVertexWithUV(wr, 0, 0, 0, uMax, vMax, skyLight, blockLight);
 		}
 	}
 }
