be0e70f5a6c092a49994a28b896ce727f2119d30
Bartek Bok <bartek.bok@gmail.com>
2017-05-29 21:14:58 +0200

    Port slimalyzer

--- src/main/java/openblocks/common/item/ItemSlimalyzer.java
+++ src/main/java/openblocks/common/item/ItemSlimalyzer.java
@@ -1,34 +1,77 @@
 package openblocks.common.item;
 
+import javax.annotation.Nullable;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 import openblocks.OpenBlocks;
 import openmods.infobook.BookDocumentation;
+import openmods.utils.ItemUtils;
 
 @BookDocumentation
 public class ItemSlimalyzer extends Item {
 
-	public ItemSlimalyzer() {}
+	// NOTE for future: world seed is not available on client side
 
-	// TODO 1.8.9 If possible, move to rendering logic?
-	@Override
-	public void onUpdate(ItemStack stack, World world, Entity entity, int par4, boolean par5) {
-		if (!world.isRemote) {
-			if (entity != null) {
-				Chunk chunk = world.getChunkFromBlockCoords(entity.getPosition());
-				int previousDamage = stack.getItemDamage();
-				stack.setItemDamage(chunk.getRandomWithSeed(987234911L).nextInt(10) == 0? 1 : 0);
-				if (previousDamage != stack.getItemDamage() && previousDamage == 0) {
-					world.playSound(null, entity.getPosition(), OpenBlocks.Sounds.ITEM_SLIMALYZER_PING, SoundCategory.PLAYERS, 1F, 1F);
-				}
-			} else {
-				stack.setItemDamage(0);
+	private static final String TAG_ACTIVE = "Active";
+
+	public ItemSlimalyzer() {
+		addPropertyOverride(new ResourceLocation("active"), new IItemPropertyGetter() {
+			@Override
+			public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn) {
+				return isActive(stack)? 2 : 0;
 			}
-		}
+		});
 	}
 
+	private static boolean isActive(ItemStack stack) {
+		final NBTTagCompound itemTag = stack.getTagCompound();
+		return itemTag != null && itemTag.getBoolean(TAG_ACTIVE);
+	}
+
+	private static boolean isInSlimeChunk(World world, Entity entity) {
+		if (world == null || entity == null) return false;
+
+		final Chunk chunk = world.getChunkFromBlockCoords(entity.getPosition());
+		return chunk.getRandomWithSeed(987234911L).nextInt(10) == 0;
+	}
+
+	private static boolean update(ItemStack stack, World world, Entity entity) {
+		final boolean isActive = isActive(stack);
+		final boolean isInSlimeChunk = isInSlimeChunk(world, entity);
+		if (isActive != isInSlimeChunk) {
+			if (isInSlimeChunk)
+				world.playSound(null, entity.getPosition(), OpenBlocks.Sounds.ITEM_SLIMALYZER_PING, SoundCategory.PLAYERS, 1F, 1F);
+			ItemUtils.getItemTag(stack).setBoolean(TAG_ACTIVE, isInSlimeChunk);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void onUpdate(ItemStack stack, World world, Entity entity, int itemSlot, boolean isSelected) {
+		if (!world.isRemote)
+			update(stack, world, entity);
+	}
+
+	@Override
+	public boolean onEntityItemUpdate(EntityItem entityItem) {
+		final World world = entityItem.worldObj;
+
+		if (!world.isRemote) {
+			final ItemStack stack = entityItem.getEntityItem();
+			if (update(stack, world, entityItem))
+				entityItem.setEntityItemStack(stack);
+		}
+
+		return false;
+	}
 }
