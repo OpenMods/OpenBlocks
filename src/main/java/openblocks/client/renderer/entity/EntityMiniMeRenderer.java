1915aa72c7db057f6e73c6042a0b4f6e279445c9
Bartek Bok <bartek.bok@gmail.com>
2017-07-21 16:15:38 +0200

    Fix mini-me rendering

--- src/main/java/openblocks/client/renderer/entity/EntityMiniMeRenderer.java
+++ src/main/java/openblocks/client/renderer/entity/EntityMiniMeRenderer.java
@@ -1,15 +1,15 @@
 package openblocks.client.renderer.entity;
 
+import net.minecraft.client.model.ModelPlayer;
 import net.minecraft.client.renderer.entity.RenderBiped;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
-import openblocks.client.model.ModelMiniMe;
 import openblocks.common.entity.EntityMiniMe;
 
 public class EntityMiniMeRenderer extends RenderBiped<EntityMiniMe> {
 
 	public EntityMiniMeRenderer(RenderManager renderManager) {
-		super(renderManager, new ModelMiniMe(0.5F), 0.5F);
+		super(renderManager, new ModelPlayer(0.5f, false), 0.5f);
 	}
 
 	@Override
