d9a4928d565df291aae1ce3ebf4589daf5def5ab
Bartek Bok <bartek.bok@gmail.com>
2017-08-06 16:43:53 +0200

    Fix item duplication in drawing table

--- src/main/java/openblocks/common/tileentity/TileEntityDrawingTable.java
+++ src/main/java/openblocks/common/tileentity/TileEntityDrawingTable.java
@@ -46,12 +46,16 @@ public class TileEntityDrawingTable extends SyncedTileEntity implements IHasGui,
 				if (MetasGeneric.unpreparedStencil.isA(input)) {
 					final ItemStack output = new ItemStack(OpenBlocks.Items.stencil, input.getCount(), selectedPattern.get().ordinal());
 					inventoryContents.set(SLOT_OUTPUT, output);
+				} else {
+					inventoryContents.set(SLOT_OUTPUT, ItemStack.EMPTY);
 				}
 			} else if (slotNumber == SLOT_OUTPUT) {
 				final ItemStack output = inventoryContents.get(SLOT_OUTPUT);
 				if (output.getItem() instanceof ItemStencil) {
 					final ItemStack input = MetasGeneric.unpreparedStencil.newItemStack(output.getCount());
 					inventoryContents.set(SLOT_INPUT, input);
+				} else {
+					inventoryContents.set(SLOT_INPUT, ItemStack.EMPTY);
 				}
 			}
 
