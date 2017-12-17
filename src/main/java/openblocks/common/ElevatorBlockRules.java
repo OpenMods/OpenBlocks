12cde2d0195a43cfe40181d059f59937e363e95d
Bartek Bok <bartek.bok@gmail.com>
2017-12-17 13:25:51 +0100

    Allow any block to be configured as elevator

--- src/main/java/openblocks/common/ElevatorBlockRules.java
+++ src/main/java/openblocks/common/ElevatorBlockRules.java
@@ -1,17 +1,25 @@
 package openblocks.common;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.EnumDyeColor;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import openblocks.Config;
+import openblocks.api.ElevatorCheckEvent;
+import openblocks.api.IElevatorBlock.PlayerRotation;
 import openmods.Log;
+import openmods.colors.ColorMeta;
 import openmods.config.properties.ConfigurationChange;
 
 public class ElevatorBlockRules {
@@ -26,6 +34,16 @@ public class ElevatorBlockRules {
 		INCREMENT
 	}
 
+	private static class ElevatorOverride {
+		public final EnumDyeColor color;
+		public final PlayerRotation rotation;
+
+		public ElevatorOverride(EnumDyeColor color, PlayerRotation rotation) {
+			this.color = color;
+			this.rotation = rotation;
+		}
+	}
+
 	private static final Map<String, Action> ACTIONS;
 
 	static {
@@ -37,6 +55,8 @@ public class ElevatorBlockRules {
 
 	private Map<Block, Action> rules;
 
+	private Map<IBlockState, ElevatorOverride> overrides;
+
 	private Map<Block, Action> getRules() {
 		if (rules == null) {
 			rules = Maps.newIdentityHashMap();
@@ -44,7 +64,7 @@ public class ElevatorBlockRules {
 				try {
 					tryAddRule(rules, entry);
 				} catch (Throwable t) {
-					Log.warn(t, "Invalid entry in map blacklist: %s", entry);
+					Log.warn(t, "Invalid entry in elevator actions: %s", entry);
 				}
 			}
 		}
@@ -69,9 +89,70 @@ public class ElevatorBlockRules {
 		else Log.warn("Can't find block %s", entry);
 	}
 
+	private Map<IBlockState, ElevatorOverride> getOverrides() {
+		if (overrides == null) {
+			overrides = Maps.newIdentityHashMap();
+			for (String entry : Config.elevatorOverrides) {
+				try {
+					tryAddOverride(overrides, entry);
+				} catch (Throwable t) {
+					Log.warn(t, "Invalid entry in elevator overrides: %s", entry);
+				}
+			}
+		}
+
+		return overrides;
+	}
+
+	private static void tryAddOverride(Map<IBlockState, ElevatorOverride> overrides, String entry) {
+		final List<String> parts = Splitter.on(';').splitToList(entry);
+		if (parts.size() == 0) return;
+
+		Preconditions.checkState(parts.size() == 1 || parts.size() == 2, "Each entry be either 'blockState' or 'blockState=color'");
+
+		final List<IBlockState> blockStates = parseBlockDesc(parts.get(0));
+
+		final EnumDyeColor color;
+		if (parts.size() == 2) {
+			final ColorMeta colorMeta = ColorMeta.fromName(parts.get(1));
+			color = colorMeta.vanillaEnum;
+		} else {
+			color = EnumDyeColor.WHITE;
+		}
+
+		for (IBlockState state : blockStates)
+			overrides.put(state, new ElevatorOverride(color, PlayerRotation.NONE));
+
+	}
+
+	private static List<IBlockState> parseBlockDesc(String blockDesc) {
+		final List<String> blockDescParts = Splitter.on('#').splitToList(blockDesc);
+
+		final String blockId = blockDescParts.get(0);
+		Block block = Block.REGISTRY.getObject(new ResourceLocation(blockId));
+		if (block == Blocks.AIR) {
+			Log.warn("Can't find block %s", blockId);
+			return Collections.emptyList();
+		}
+
+		if (blockDescParts.size() == 1) {
+			return block.getBlockState().getValidStates();
+		} else {
+			final String stateDesc = blockDescParts.get(1);
+			return decodeState(block, stateDesc);
+		}
+	}
+
+	@SuppressWarnings("deprecation")
+	private static List<IBlockState> decodeState(Block block, final String stateDesc) {
+		final int meta = Integer.parseInt(stateDesc);
+		return ImmutableList.of(block.getStateFromMeta(meta));
+	}
+
 	@SubscribeEvent
 	public void onReconfig(ConfigurationChange.Post evt) {
 		if (evt.check("dropblock", "specialBlockRules")) rules = null;
+		if (evt.check("dropblock", "overrides")) overrides = null;
 	}
 
 	private static boolean isPassable(IBlockState state) {
@@ -83,6 +164,15 @@ public class ElevatorBlockRules {
 		if (action != null) return action;
 
 		return isPassable(state)? Action.IGNORE : Action.INCREMENT;
+	}
+
+	public void configureEvent(ElevatorCheckEvent evt) {
+		final Map<IBlockState, ElevatorOverride> overrides = getOverrides();
+		final ElevatorOverride elevatorOverride = overrides.get(evt.getState());
+		if (elevatorOverride != null) {
+			evt.setColor(elevatorOverride.color);
+			evt.setRotation(elevatorOverride.rotation);
+		}
 	}
 
 }
--- src/main/java/openblocks/events/ElevatorActionEvent.java
+++ src/main/java/openblocks/events/ElevatorActionEvent.java
@@ -1,19 +1,17 @@
 package openblocks.events;
 
 import net.minecraft.network.PacketBuffer;
-import net.minecraft.util.math.BlockPos;
-import openmods.events.network.BlockEventPacket;
 import openmods.movement.PlayerMovementEvent;
 import openmods.network.event.EventDirection;
+import openmods.network.event.NetworkEvent;
 import openmods.network.event.NetworkEventMeta;
 
 @NetworkEventMeta(direction = EventDirection.C2S)
-public class ElevatorActionEvent extends BlockEventPacket {
+public class ElevatorActionEvent extends NetworkEvent {
 
 	public ElevatorActionEvent() {}
 
-	public ElevatorActionEvent(int dimension, BlockPos pos, PlayerMovementEvent.Type type) {
-		super(dimension, pos);
+	public ElevatorActionEvent(PlayerMovementEvent.Type type) {
 		this.type = type;
 	}
 
@@ -21,13 +19,11 @@ public class ElevatorActionEvent extends BlockEventPacket {
 
 	@Override
 	protected void readFromStream(PacketBuffer input) {
-		super.readFromStream(input);
 		type = input.readEnumValue(PlayerMovementEvent.Type.class);
 	}
 
 	@Override
 	protected void writeToStream(PacketBuffer output) {
-		super.writeToStream(output);
 		output.writeEnumValue(type);
 	}
 }
