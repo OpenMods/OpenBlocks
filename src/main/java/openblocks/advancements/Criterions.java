078f084afaeb648d91c09d683eb295211416200b
Bartek Bok <bartek.bok@gmail.com>
2018-04-08 12:52:42 +0200

    Simplify advancements' criteria registration

--- src/main/java/openblocks/advancements/Criterions.java
+++ src/main/java/openblocks/advancements/Criterions.java
@@ -1,23 +1,16 @@
 package openblocks.advancements;
 
 import net.minecraft.advancements.CriteriaTriggers;
-import net.minecraft.advancements.ICriterionTrigger;
-import openmods.reflection.MethodAccess;
-import openmods.reflection.MethodAccess.Function1;
 
 public class Criterions {
-
-	@SuppressWarnings("rawtypes")
-	private static final Function1<ICriterionTrigger, ICriterionTrigger> register =
-			MethodAccess.create(ICriterionTrigger.class, CriteriaTriggers.class, ICriterionTrigger.class, "func_192118_a", "register");
 
 	public static TriggerDevNullStack devNullStack = new TriggerDevNullStack();
 
 	public static TriggerBrickDropped brickDropped = new TriggerBrickDropped();
 
 	public static void init() {
-		register.call(null, devNullStack);
-		register.call(null, brickDropped);
+		CriteriaTriggers.register(devNullStack);
+		CriteriaTriggers.register(brickDropped);
 	}
 
 }
