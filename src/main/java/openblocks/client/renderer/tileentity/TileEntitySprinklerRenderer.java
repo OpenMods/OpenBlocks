517c954cb18284b9f1910c308c24137fbc912adb
Bartek Bok <bartek.bok@gmail.com>
2018-04-28 16:41:24 +0200

    Fix #909

--- src/main/java/openblocks/client/renderer/tileentity/TileEntitySprinklerRenderer.java
+++ src/main/java/openblocks/client/renderer/tileentity/TileEntitySprinklerRenderer.java
@@ -4,12 +4,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockRendererDispatcher;
 import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.RenderHelper;
-import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.block.model.IBakedModel;
-import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.client.MinecraftForgeClient;
@@ -18,14 +13,13 @@ import net.minecraftforge.common.property.IExtendedBlockState;
 import net.minecraftforge.common.property.Properties;
 import openblocks.common.tileentity.TileEntitySprinkler;
 import openmods.model.eval.EvalModelState;
-import org.lwjgl.opengl.GL11;
 
 public class TileEntitySprinklerRenderer extends FastTESR<TileEntitySprinkler> {
 
 	protected static BlockRendererDispatcher blockRenderer;
 
 	@Override
-	public void renderTileEntityFast(TileEntitySprinkler te, double x, double y, double z, float partialTick, int breakStage, float alpha, BufferBuilder renderer) {
+	public void renderTileEntityFast(TileEntitySprinkler te, double x, double y, double z, float partialTick, int breakStage, float alpha, BufferBuilder vb) {
 		if (blockRenderer == null) blockRenderer = Minecraft.getMinecraft().getBlockRendererDispatcher();
 
 		final BlockPos pos = te.getPos();
@@ -38,22 +32,6 @@ public class TileEntitySprinklerRenderer extends FastTESR<TileEntitySprinkler> {
 		if (state instanceof IExtendedBlockState) {
 			IExtendedBlockState exState = (IExtendedBlockState)state;
 			if (exState.getUnlistedNames().contains(EvalModelState.PROPERTY)) {
-				Tessellator tessellator = Tessellator.getInstance();
-				BufferBuilder vb = tessellator.getBuffer();
-				bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-				RenderHelper.disableStandardItemLighting();
-				GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-				GlStateManager.enableBlend();
-				GlStateManager.disableCull();
-
-				if (Minecraft.isAmbientOcclusionEnabled()) {
-					GlStateManager.shadeModel(GL11.GL_SMOOTH);
-				} else {
-					GlStateManager.shadeModel(GL11.GL_FLAT);
-				}
-
-				vb.begin(GL11.GL_QUADS, DefaultVertexFormats.BLOCK);
-
 				exState = exState.withProperty(EvalModelState.PROPERTY, te.getRenderState());
 
 				vb.setTranslation(x - pos.getX(), y - pos.getY(), z - pos.getZ());
@@ -62,10 +40,6 @@ public class TileEntitySprinklerRenderer extends FastTESR<TileEntitySprinkler> {
 				blockRenderer.getBlockModelRenderer().renderModel(world, model, exState, pos, vb, false);
 
 				vb.setTranslation(0, 0, 0);
-
-				tessellator.draw();
-
-				RenderHelper.enableStandardItemLighting();
 			}
 		}
 	}
