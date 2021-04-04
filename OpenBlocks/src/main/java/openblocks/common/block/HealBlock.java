65b832e7aade87d6eb0661bfffaff8db73a7f2e1
agnor99 <36055315+agnor99@users.noreply.github.com>
2021-04-04 11:36:28 +0200

    Healer block 1.16 (#1034)
    Ported HealerBlock

---
+++ OpenBlocks/src/main/java/openblocks/common/block/HealBlock.java
@@ -0,0 +1,9 @@
+package openblocks.common.block;
+
+import openmods.block.OpenBlock;
+
+public class HealBlock extends OpenBlock {
+    public HealBlock(Properties properties) {
+        super(properties);
+    }
+}
---
+++ OpenBlocks/src/main/java/openblocks/common/tileentity/HealTileEntity.java
@@ -0,0 +1,35 @@
+package openblocks.common.tileentity;
+
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.tileentity.ITickableTileEntity;
+import openblocks.OpenBlocks;
+import openmods.tileentity.OpenTileEntity;
+import openmods.utils.BlockUtils;
+
+import java.util.List;
+
+public class HealTileEntity extends OpenTileEntity implements ITickableTileEntity {
+    public HealTileEntity() {
+        super(OpenBlocks.TileEntities.heal);
+    }
+
+    @Override
+    public void tick() {
+        if (world.isRemote) return;
+        if (world.getGameTime() % 10 == 0) {
+            List<ServerPlayerEntity> playersOnTop = world.getEntitiesWithinAABB(ServerPlayerEntity.class, BlockUtils.expandAround(pos, 1, 2, 1));
+            for (ServerPlayerEntity player : playersOnTop) {
+                if (player.interactionManager.getGameType().isSurvivalOrAdventure()) {
+                    //Just for the effectDisplay
+                    player.addPotionEffect(new EffectInstance(Effects.REGENERATION, 15, 1));
+                    player.addPotionEffect(new EffectInstance(Effects.SATURATION, 15, -1));
+                    //Change only happens here
+                    player.getFoodStats().addStats(1,1);
+                    player.heal(1);
+                }
+            }
+        }
+    }
+}
